cb6f627034d687a28d8c37499e341d8340ee7817
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-03-17 21:12:47 +0000

    blog

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,17 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ReactDOM from "react-dom";
+import { Navbar } from "./navbar.js";
+import { Homepage } from "./homepage.js";
+import { BlogList } from "./blog-list.js";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<Navbar />
+			<div className="content">
+				<Homepage />
+			</div>
 		</div>
 	);
 }
